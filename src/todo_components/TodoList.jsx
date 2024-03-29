8e3f57b27d38e5bc098a53c30f632d54f85806da
Daniel Park <ape072399@gmail.com>
2024-03-29 11:53:11 +0900

    TodoList

---
+++ src/todo_components/TodoList.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import '../apps/App.css';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { v4 as uuidv4 } from 'uuid';
+import AddTodo from './AddTodo';
+import Todo from './Todo';
+
+// props로 보내고 콜백함수로 받기
+export default function TodoList() {
+
+  const [todos, setTodos] = useState([
+    {id:uuidv4(), work:'공부하기', status:'active'},
+    {id:uuidv4(), work:'청소하기', status:'active'},
+  ]);
+  const handleUpdate = updated => 
+    setTodos(todos.map(todo => (todo.id === updated.id) ? updated : todo));
+  const handleDelete = deleted =>
+    setTodos(todos.filter(todo => todo.id !== deleted.id));
+  const handleAdd = todo => setTodos([...todos, todo]);
+
+  return (
+    <div>
+      <ul>
+        {
+          todos.map(todo => (
+            <Todo todo={todo} onUpdate={handleUpdate} onDelete={handleDelete} />
+          ))
+        }
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </div>
+  );
+}
